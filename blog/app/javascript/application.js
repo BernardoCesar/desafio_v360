809fe40a623ea9c3e3a8b40a45fcb03a90e512f8
BernardoCesar <bernardocesar2002@gmail.com>
2023-03-14 19:47:03 -0300

    css

--- blog/app/javascript/application.js
+++ blog/app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+var botaoFeito=document.getElementById("feito");
+var item=document.getElementById("itemFeito")
+
+    function todoFeito(){
+        item=document.getElementById("itemFeito").className="feito";
+    }
+
+botaoFeito.onclick=todoFeito;
